51cbf7bf8838a7042bf4aadacd3d7a346b87712c
Mohanraj S <144809670+MohanrajS07@users.noreply.github.com>
2023-11-05 19:00:29 +0530

    OIBSIP Task 2.java

---
+++ OIBSIP Task 2.java
@@ -0,0 +1,90 @@
+package OIBSIP;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class Game {
+
+    public int number;
+    public int inputNumber;
+    public int noOfGuesses = 0;
+
+    public int getNoOfGuesses() {
+
+        return noOfGuesses;
+    }
+
+    public void setNoOfGuesses(int noOfGuesses) {
+        this.noOfGuesses = noOfGuesses;
+    }
+
+    Game() {
+        Random rand = new Random();
+        this.number = rand.nextInt(100);
+    }
+
+    void takeUserInput() {
+        System.out.println("Guess the Correct number");
+        Scanner sc = new Scanner(System.in);
+        inputNumber = sc.nextInt();
+    }
+
+    Boolean isCorrectNumber() {
+
+        noOfGuesses++;
+        if (inputNumber == number && noOfGuesses < 6) {
+            System.out.format("Yes you guessed it right, it was %d\nYou guessed it in %d attempt", number, noOfGuesses);
+            System.out.println(" ");
+
+            return true;
+        } else if (noOfGuesses == 5) {
+            System.out.println("Your Attempts are over You lost the Game ");
+            return true;
+        } else if (inputNumber < number && noOfGuesses < 5) {
+            System.out.println("Too low");
+        } else if (inputNumber > number && noOfGuesses < 5) {
+            System.out.println("Too high");
+        }
+
+        return false;
+    }
+
+}
+public class Number_Guessing_Game {
+
+	public static void main(String[] args) {
+
+		        Scanner sc = new Scanner(System.in);
+		        System.out.println("If you want to Play the Game Press 1 or 0");
+		        int count = 0;
+		        int s = sc.nextInt();
+		        while (true) {
+		            while (s == 1) {
+		                Game g = new Game();
+		                boolean b = false;
+		                while (!b) {
+		                    g.takeUserInput();
+		                    b = g.isCorrectNumber();
+
+		                }
+		                count++;
+		                System.out.println("Total no of round You played:" + count);
+		                System.out.println("------------------------------------------------------------------------------------------------");
+		                System.out.println("If you want to Play the Game Press 1 or 0");
+
+		                s = sc.nextInt();
+		            }
+		            while (s == 0) {
+		                System.out.println("Thank You ");
+		                System.exit(0);
+
+		            }
+		            while (s != 0 && s != 1) {
+		                System.out.println("Choose right option");
+		                s = sc.nextInt();
+		            }
+		        }
+
+	}
+
+}
